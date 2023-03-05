e54364297fa5f32edc9fb84df7bfe6206ce57536
unknown <danasheer21111@gmail.com>
2023-03-05 20:33:37 +0300

    still

--- reciepes/admin.py
+++ reciepes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Reciepe, Ingredient
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Reciepe)
+admin.site.register(Ingredient)
--- reciepes/models.py
+++ reciepes/models.py
@@ -8,30 +8,33 @@ User = get_user_model()
 class Category(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='category/')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Reciepe(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='reciepe/')
     description = models.TextField()
-    ingredients = models.TextField()
     steps = models.TextField()
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reciepe')
+        User, on_delete=models.CASCADE, related_name='reciepies')
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='reciepe')
+        Category, on_delete=models.CASCADE, related_name='reciepies')
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.title
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
     reciepe = models.ForeignKey(
-        Reciepe, on_delete=models.CASCADE, related_name='ingredient')
+        Reciepe, on_delete=models.CASCADE, related_name='ingredients')
+    user_ingredient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
         return self.name
